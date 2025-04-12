efc9c5434ef2f7b3b4c1cfcdfa3f0d984bf9846b
AbhinavMisra69 <65548564+AbhinavMisra69@users.noreply.github.com>
2025-04-12 19:21:36 +0530

    Update north_indian_cities.cpp

--- north_indian_cities.cpp
+++ north_indian_cities.cpp
@@ -1,21 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <cmath>
+#include <limits>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
-class Node {
-public:
+using namespace std;
+
+struct City {
     int id;
-    std::string name;
+    string name;
     int x, y;
 
-    Node(int id, const std::string& name, int x, int y)
+    City(int id, const string& name, int x, int y)
         : id(id), name(name), x(x), y(y) {}
 };
 
-double euclidean(const Point& a, const Point& b) {
-    return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
+double euclidean(const City& a, const City& b) {
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
-
 
 vector<vector<City>> kMeansClustering(const vector<City>& cities, int k, double& total_wcss) {
     int n = cities.size();
@@ -25,7 +29,7 @@ vector<vector<City>> kMeansClustering(const vector<City>& cities, int k, double&
 
     srand(time(0));
 
-    // Randomly initialize centroids
+    // Initialize centroids randomly
     for (int i = 0; i < k; ++i)
         centroids[i] = cities[rand() % n];
 
@@ -35,11 +39,9 @@ vector<vector<City>> kMeansClustering(const vector<City>& cities, int k, double&
     while (changed && max_iters--) {
         changed = false;
 
-        // Clear previous cluster data
         for (int i = 0; i < k; ++i)
             clusters[i].clear();
 
-        // Assign cities to nearest centroid
         for (int i = 0; i < n; ++i) {
             double min_dist = numeric_limits<double>::max();
             int best_cluster = 0;
@@ -60,7 +62,7 @@ vector<vector<City>> kMeansClustering(const vector<City>& cities, int k, double&
             clusters[best_cluster].push_back(cities[i]);
         }
 
-        // Update centroids
+        // Recalculate centroids
         for (int i = 0; i < k; ++i) {
             double sum_x = 0, sum_y = 0;
             int cluster_size = clusters[i].size();
@@ -77,7 +79,7 @@ vector<vector<City>> kMeansClustering(const vector<City>& cities, int k, double&
         }
     }
 
-    // Calculate total WCSS
+    // Compute total WCSS
     total_wcss = 0;
     for (int i = 0; i < k; ++i) {
         for (const City& c : clusters[i]) {
@@ -88,68 +90,28 @@ vector<vector<City>> kMeansClustering(const vector<City>& cities, int k, double&
     return clusters;
 }
 
-
 int main() {
-    Node delhi(1, "Delhi", 700, 220);
-    Node amritsar(2, "Amritsar", 640, 130);
-    Node chandigarh(3, "Chandigarh", 670, 150);
-    Node jaipur(4, "Jaipur", 600, 350);
-    Node lucknow(5, "Lucknow", 690, 320);
-    Node kanpur(6, "Kanpur", 675, 340);
-    Node agra(7, "Agra", 670, 275);
-    Node varanasi(8, "Varanasi", 750, 375);
-    Node meerut(9, "Meerut", 720, 250);
-    Node aligarh(10, "Aligarh", 690, 260);
-    Node patna(11, "Patna", 770, 410);
-    Node ghaziabad(12, "Ghaziabad", 715, 230);
-    Node moradabad(13, "Moradabad", 730, 265);
-    Node bareilly(14, "Bareilly", 705, 280);
-    Node saharanpur(15, "Saharanpur", 690, 205);
-    Node haridwar(16, "Haridwar", 670, 190);
-    Node roorkee(17, "Roorkee", 665, 180);
-    Node rishikesh(18, "Rishikesh", 660, 170);
-    Node nainital(19, "Nainital", 655, 150);
-    Node mathura(20, "Mathura", 680, 265);
-    Node hoshiarpur(21, "Hoshiarpur", 645, 140);
-    Node kullu(22, "Kullu", 655, 120);
-    Node shimla(23, "Shimla", 660, 110);
-    Node kangra(24, "Kangra", 650, 125);
-    Node solan(25, "Solan", 660, 130);
-    Node srinagar(26, "Srinagar", 610, 90);
-    Node jammu(27, "Jammu", 625, 100);
-    Node ludhiana(28, "Ludhiana", 660, 180);
-    Node patiala(29, "Patiala", 650, 170);
-    Node panipat(30, "Panipat", 705, 240);
-    Node sonipat(31, "Sonipat", 710, 230);
-    Node muzaffarnagar(32, "Muzaffarnagar", 720, 260);
-    Node fatehpur(33, "Fatehpur", 690, 310);
-    Node karnal(34, "Karnal", 705, 230);
-    Node bhiwani(35, "Bhiwani", 680, 245);
-    Node hisar(36, "Hisar", 670, 275);
-    Node jind(37, "Jind", 660, 280);
-    Node kurukshetra(38, "Kurukshetra", 715, 220);
-    Node rohtak(39, "Rohtak", 720, 230);
-    Node faridabad(40, "Faridabad", 700, 215);
-    Node barnala(41, "Barnala", 640, 150);
-    Node muktsar(42, "Muktsar", 635, 135);
-    Node sangrur(43, "Sangrur", 630, 140);
-    Node bhatinda(44, "Bhatinda", 625, 160);
-    Node jalandhar(45, "Jalandhar", 650, 160);
-    Node ambala(46, "Ambala", 675, 160);
-    Node gurugram(47, "Gurugram", 710, 210);
-    Node noida(48, "Noida", 715, 220);
-    Node farukhabad(49, "Farukhabad", 755, 360);
+    vector<City> cities = {
+        {1, "Delhi", 700, 220}, {2, "Amritsar", 640, 130}, {3, "Chandigarh", 670, 150},
+        {4, "Jaipur", 600, 350}, {5, "Lucknow", 690, 320}, {6, "Kanpur", 675, 340},
+        {7, "Agra", 670, 275}, {8, "Varanasi", 750, 375}, {9, "Meerut", 720, 250},
+        {10, "Aligarh", 690, 260}, {11, "Patna", 770, 410}, {12, "Ghaziabad", 715, 230},
+        {13, "Moradabad", 730, 265}, {14, "Bareilly", 705, 280}, {15, "Saharanpur", 690, 205},
+        {16, "Haridwar", 670, 190}, {17, "Roorkee", 665, 180}, {18, "Rishikesh", 660, 170},
+        {19, "Nainital", 655, 150}, {20, "Mathura", 680, 265}, {21, "Hoshiarpur", 645, 140},
+        {22, "Kullu", 655, 120}, {23, "Shimla", 660, 110}, {24, "Kangra", 650, 125},
+        {25, "Solan", 660, 130}, {26, "Srinagar", 610, 90}, {27, "Jammu", 625, 100},
+        {28, "Ludhiana", 660, 180}, {29, "Patiala", 650, 170}, {30, "Panipat", 705, 240},
+        {31, "Sonipat", 710, 230}, {32, "Muzaffarnagar", 720, 260}, {33, "Fatehpur", 690, 310},
+        {34, "Karnal", 705, 230}, {35, "Bhiwani", 680, 245}, {36, "Hisar", 670, 275},
+        {37, "Jind", 660, 280}, {38, "Kurukshetra", 715, 220}, {39, "Rohtak", 720, 230},
+        {40, "Faridabad", 700, 215}, {41, "Barnala", 640, 150}, {42, "Muktsar", 635, 135},
+        {43, "Sangrur", 630, 140}, {44, "Bhatinda", 625, 160}, {45, "Jalandhar", 650, 160},
+        {46, "Ambala", 675, 160}, {47, "Gurugram", 710, 210}, {48, "Noida", 715, 220},
+        {49, "Farukhabad", 755, 360}
+    };
 
-    std::vector<Node> cities = {delhi, amritsar, chandigarh, jaipur, lucknow, kanpur, agra, varanasi, meerut, aligarh, patna, ghaziabad, moradabad, bareilly, saharanpur, haridwar, roorkee, rishikesh, nainital, mathura, hoshiarpur, kullu, shimla, kangra, solan, srinagar, jammu, ludhiana, patiala, panipat, sonipat, muzaffarnagar, fatehpur, karnal, bhiwani, hisar, jind, kurukshetra, rohtak, faridabad, barnala, muktsar, sangrur, bhatinda, jalandhar, ambala, gurugram, noida, farukhabad};
-
-    // Example: print city names
-    for (const Node& city : cities) {
-        std::cout << "City: " << city.name << " (ID: " << city.id
-                  << ", X: " << city.x << ", Y: " << city.y << ")\n";
-    }
-
-
-     int k = 10;
+    int k = 10;
     double wcss = 0;
     auto clusters = kMeansClustering(cities, k, wcss);
 
@@ -162,6 +124,5 @@ int main() {
     }
 
     cout << "Total WCSS: " << wcss << endl;
-
     return 0;
 }
