be506f60270920ffaf947739a44a28e829767749
AbhinavMisra69 <65548564+AbhinavMisra69@users.noreply.github.com>
2025-05-03 19:11:49 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <iostream>
 #include <limits>
+#include <unordered_map>
 using namespace std;
 
 struct Node {
@@ -15,50 +16,68 @@ struct Node {
 struct Vehicle {
     int capacity;
     int load = 0;
-    vector<int> route; // list of node IDs, starting and ending at depot (0)
+    int depotID;
+    vector<int> route; // customer node IDs only
 };
 
 double Distance(const Node& a, const Node& b) {
     return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
 }
 
-double RouteCost(const vector<int>& route, const vector<Node>& nodes) {
+double RouteCost(const vector<int>& route, const vector<Node>& nodes, const Node& depot) {
     double cost = 0.0;
+    cost += Distance(depot, nodes[route.front()]);
     for (int i = 0; i < route.size() - 1; ++i)
         cost += Distance(nodes[route[i]], nodes[route[i+1]]);
+    cost += Distance(nodes[route.back()], depot);
     return cost;
 }
 
-double TotalCost(const vector<Vehicle>& vehicles, const vector<Node>& nodes) {
+double TotalCost(const vector<Vehicle>& vehicles, const vector<Node>& nodes, const unordered_map<int, Node>& depots) {
     double total = 0.0;
     for (const auto& v : vehicles)
-        total += RouteCost(v.route, nodes);
+        total += RouteCost(v.route, nodes, depots.at(v.depotID));
     return total;
 }
 
-// Create a simple initial solution (greedy split by capacity)
-vector<Vehicle> CreateInitialSolution(const vector<Node>& nodes, int vehicleCount, int vehicleCap) {
-    vector<Vehicle> vehicles(vehicleCount, {vehicleCap});
-    int v = 0;
-    for (int i = 1; i < nodes.size(); ++i) {
-        if (vehicles[v].load + nodes[i].demand > vehicleCap) {
-            v++;
-            if (v >= vehicleCount) break;
-        }
-        vehicles[v].load += nodes[i].demand;
-        vehicles[v].route.push_back(i);
+vector<Vehicle> CreateInitialSolution(const vector<Node>& nodes, const vector<int>& depotIDs, int vehiclesPerDepot, int vehicleCap) {
+    unordered_map<int, Node> depots;
+    for (int id : depotIDs)
+        depots[id] = nodes[id];
+
+    vector<Node> customers;
+    for (const auto& node : nodes)
+        if (find(depotIDs.begin(), depotIDs.end(), node.id) == depotIDs.end())
+            customers.push_back(node);
+
+    vector<Vehicle> vehicles;
+    int depotIdx = 0;
+    for (int d : depotIDs) {
+        for (int v = 0; v < vehiclesPerDepot; ++v)
+            vehicles.push_back({vehicleCap, 0, d, {}});
     }
-    for (auto& veh : vehicles) {
-        veh.route.insert(veh.route.begin(), 0); // start at depot
-        veh.route.push_back(0); // end at depot
+
+    int vi = 0;
+    for (const auto& cust : customers) {
+        while (vi < vehicles.size() && vehicles[vi].load + cust.demand > vehicleCap)
+            vi++;
+        if (vi >= vehicles.size()) break;
+
+        vehicles[vi].load += cust.demand;
+        vehicles[vi].route.push_back(cust.id);
     }
+
     return vehicles;
 }
 
-vector<Vehicle> SimulatedAnnealingVRP(const vector<Node>& nodes, int vehicleCount, int vehicleCap) {
-    vector<Vehicle> current = CreateInitialSolution(nodes, vehicleCount, vehicleCap);
+vector<Vehicle> SimulatedAnnealingVRP(const vector<Node>& nodes, const vector<int>& depotIDs, int vehiclesPerDepot, int vehicleCap) {
+    unordered_map<int, Node> depots;
+    for (int id : depotIDs)
+        depots[id] = nodes[id];
+
+    vector<Vehicle> current = CreateInitialSolution(nodes, depotIDs, vehiclesPerDepot, vehicleCap);
     vector<Vehicle> best = current;
-    double bestCost = TotalCost(best, nodes);
+    double bestCost = TotalCost(best, nodes, depots);
     double temp = 1000.0;
     double cooling = 0.995;
     int maxIter = 10000;
@@ -68,17 +87,16 @@ vector<Vehicle> SimulatedAnnealingVRP(const vector<Node>& nodes, int vehicleCoun
     for (int iter = 0; iter < maxIter; ++iter) {
         vector<Vehicle> neighbor = current;
 
-        // Swap two customers between two random vehicles
-        int v1 = rand() % vehicleCount;
-        int v2 = rand() % vehicleCount;
-        if (neighbor[v1].route.size() <= 2 || neighbor[v2].route.size() <= 2) continue;
+        int v1 = rand() % neighbor.size();
+        int v2 = rand() % neighbor.size();
+        if (neighbor[v1].route.empty() || neighbor[v2].route.empty()) continue;
 
-        int i = 1 + rand() % (neighbor[v1].route.size() - 2);
-        int j = 1 + rand() % (neighbor[v2].route.size() - 2);
+        int i = rand() % neighbor[v1].route.size();
+        int j = rand() % neighbor[v2].route.size();
 
         swap(neighbor[v1].route[i], neighbor[v2].route[j]);
 
-        double newCost = TotalCost(neighbor, nodes);
+        double newCost = TotalCost(neighbor, nodes, depots);
         double delta = newCost - bestCost;
 
         if (delta < 0 || (rand() / (double)RAND_MAX) < exp(-delta / temp)) {
